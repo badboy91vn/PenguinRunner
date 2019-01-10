c1c8949a6b56d867a3bf9df4d646d54161365e78
HNL-PC <bad.boy91vn@yahoo.com>
2019-01-11 00:30:56 +0700

    HNL_PC

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using GooglePlayGames;
+﻿using System;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi.SavedGame;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,15 +20,16 @@ public class GameManager : MonoBehaviour
     // UI
     public Animator gameMenuAnim;
     public Animator menuAnim;
-    public Text highScoreText;
+    public Text highScoreText, totalCoinText;
     public Text scoreText, modifierText, coinText, diamonText;
-    private float score, modifierScore, coin, diamon;
-    private int lastScore;
+    private float score, modifierScore;
+    private int coin, diamon, lastScore, totalCoin;
     // -- Death Menu
     public Animator deathMenuAnim;
     public Text deathScoreText, deathCoinText, deathDiamonText;
     // -- GPGS Menu
     public GameObject connectedGPGSBTN, disconnectedGPGSBTN;
+    public Text gpgsText;
 
     void Awake()
     {
@@ -48,6 +51,8 @@ public class GameManager : MonoBehaviour
         highScoreText.text = PlayerPrefs.GetInt("Highscore").ToString();
 
         // GPGS
+        GooglePlayGames.BasicApi.PlayGamesClientConfiguration config = new GooglePlayGames.BasicApi.PlayGamesClientConfiguration.Builder().EnableSavedGames().Build();
+        PlayGamesPlatform.InitializeInstance(config);
         PlayGamesPlatform.Activate();
         OnConnectionResponse(PlayGamesPlatform.Instance.localUser.authenticated);
     }
@@ -79,6 +84,7 @@ public class GameManager : MonoBehaviour
         //}
     }
 
+    // Update Text
     public void UpdateScore()
     {
         scoreText.text = "Score: " + score.ToString("0");
@@ -136,27 +142,57 @@ public class GameManager : MonoBehaviour
 
             PlayerPrefs.SetInt("Highscore", (int)s);
         }
+
+        totalCoin += coin;
+        OpenSave(true);
     }
 
     // Google Play Services
     public void OnClickConnectGPGS()
     {
-        Social.localUser.Authenticate((bool success) => { OnConnectionResponse(success); });
+        Social.localUser.Authenticate((bool success) =>
+        {
+            print("Authen: " + success.ToString());
+            OnConnectionResponse(success);
+        });
     }
     private void OnConnectionResponse(bool authenticated)
     {
         print(authenticated);
+        gpgsText.text = authenticated.ToString();
         if (authenticated)
         {
             UnlockAchievement(PR_GPGS.achievement_login);
             connectedGPGSBTN.SetActive(true);
             disconnectedGPGSBTN.SetActive(false);
+            OpenSave(false);
         }
         else
         {
             connectedGPGSBTN.SetActive(false);
             disconnectedGPGSBTN.SetActive(true);
         }
+    }
+
+    private string GetSaveString()
+    {
+        string str = "";
+        str += PlayerPrefs.GetInt("Highscore").ToString();
+        str += "|";
+        str += totalCoin.ToString();
+
+        return str; // 100|80
+    }
+
+    private void LoadSaveString(string save)
+    {
+        // 100|80
+        string[] data = save.Split('|');
+
+        PlayerPrefs.SetInt("Highscore", int.Parse(data[0]));
+        totalCoin = int.Parse(data[1]);
+
+        totalCoinText.text = totalCoin.ToString();
     }
 
     // Leadboard
@@ -191,4 +227,51 @@ public class GameManager : MonoBehaviour
         });
     }
 
+    // Cloud Saving
+    private bool isSaving = false;
+    public void OpenSave(bool saving)
+    {
+        Debug.Log("Open Save");
+        if (Social.localUser.authenticated)
+        {
+            isSaving = saving;
+            ((PlayGamesPlatform)Social.Active).SavedGame
+                .OpenWithAutomaticConflictResolution(
+                    "PenguinRunner",
+                    GooglePlayGames.BasicApi.DataSource.ReadCacheOrNetwork,
+                    GooglePlayGames.BasicApi.SavedGame.ConflictResolutionStrategy.UseLongestPlaytime, SaveGameOpened);
+        }
+    }
+    private void SaveGameOpened(SavedGameRequestStatus status, ISavedGameMetadata meta)
+    {
+        Debug.Log("SaveGameOpened");
+        if (status == SavedGameRequestStatus.Success)
+        {
+            if (isSaving) // writing
+            {
+                byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(GetSaveString());
+                SavedGameMetadataUpdate update = new SavedGameMetadataUpdate.Builder().WithUpdatedDescription("Save at " + DateTime.Now.ToString()).Build();
+
+                ((PlayGamesPlatform)Social.Active).SavedGame.CommitUpdate(meta, update, data, SaveUpdate);
+            }
+            else // reading
+            {
+                ((PlayGamesPlatform)Social.Active).SavedGame.ReadBinaryData(meta, SaveRead);
+            }
+        }
+    }
+    // Load
+    private void SaveRead(SavedGameRequestStatus status, byte[] data)
+    {
+        if (status == SavedGameRequestStatus.Success)
+        {
+            string saveData = System.Text.ASCIIEncoding.ASCII.GetString(data);
+            LoadSaveString(saveData);
+        }
+    }
+    // Success Save
+    private void SaveUpdate(SavedGameRequestStatus status, ISavedGameMetadata meta)
+    {
+        throw new NotImplementedException();
+    }
 }
